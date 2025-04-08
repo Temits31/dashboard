d42777e86103b84125d429e7fa0ad60cf3266dde
Ezra Padua <132443010+immortalrevenge@users.noreply.github.com>
2025-04-08 13:42:44 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Reports from './Reports';
 const App = () => {
   return (
     <div>
-      <div className="fixed top-0">
+      <div className="fixed top-0 z-50">
             <NavBar />
           </div>
       <div className="">
