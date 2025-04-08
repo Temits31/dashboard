aa6307862cdaed6d8ca8bce837d4de3149b95632
Ezra Padua <132443010+immortalrevenge@users.noreply.github.com>
2025-04-08 13:30:39 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 
 import EmployeeCard from "./EmployeeCard"
 import NavBar from './NavBar';
+import Reports from './Reports';
 
 
 const App = () => {
   return (
     <div>
-      <div>
-        <NavBar/>
-      </div>
+      <div className="fixed top-0">
+            <NavBar />
+          </div>
       <div className="">
         <EmployeeCard />
+        <div className="max-w-full">
+          <Reports />
+        </div>
       </div>
     </div>
   )
